82a98292b3fcf87dc99456d1aaf90b006b8b2b86
loay <loayq55@gmail.com>
2021-10-26 16:04:07 +0300

    Added a defalt value

--- javaHello.java
+++ javaHello.java
@@ -1,6 +1,11 @@
  public class Hello{
-     public static void main(String[] args)
+     public static void main(String args[])
 	 {
-        System.out.println("Hello, World!");
+        String name ="World";
+		if (argv.length != 0)
+		{
+			name = args[0];
+		}
+		System.out.println("Hello "+name+" !")
      }
  }
